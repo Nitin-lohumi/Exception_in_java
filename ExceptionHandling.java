81605ca7477e9a3b9cb14cb090e6bee6db637670
Nitin Lohumi <140873160+Nitin-lohumi@users.noreply.github.com>
2024-05-09 09:10:52 +0530

    Add files via upload

---
+++ ExceptionHandling.java
@@ -0,0 +1,33 @@
+class Exe {
+  static int age;
+  @SuppressWarnings("static-access")
+  Exe(int a){
+    this.age= a;
+  }
+  static void msg() throws CreateException{
+     if(age<18){
+      try{
+        throw new CreateException("age is under 18");
+      }
+      catch(CreateException e){
+        System.out.println("exception occoured");
+        throw e;
+      }
+     }
+    else{
+      System.out.println("allowed ");
+    }
+  }
+}
+public class ExceptionHandling {
+  public static void main(String[] args){
+  @SuppressWarnings("unused")
+  Exe n = new Exe(17);
+  try {
+    Exe.msg();
+  }catch(CreateException e){
+   System.out.println(" alert age in less then 18");
+   System.out.println(e);
+  }
+}
+}
